4756a6f28cab357a0c462c88c1d646427713be38
Per-Arne Andersen <per@sysx.no>
2018-10-02 10:46:43 +0200

    * Added per-rl as a submodule * Updated config to contain explicit dict declaration * Moded examples to the deep_line_wars_examples package

--- deep_line_wars/config.py
+++ deep_line_wars/config.py
@@ -1,6 +1,6 @@
 from deep_line_wars.gui import dummy
 
-default_config = dict(
+default_config =dict(
   game=dict(
     width=11,
     height=11,
@@ -24,50 +24,50 @@ default_config = dict(
   ),
     gui=dummy.GUI,
     gui_draw_friendly=True,
-    state_representation="RGB"  # "RAW", "RGB", "L"
+    state_representation="RGB"  # RAW, RGB, L
 )
 
 building=[
-    {
-        "id": 1,
-        "name": "Basic-Tower",
-        "icon": "tower_1.png",
-        "health": 100,
-        "attack_min": 2,
-        "attack_max": 4,
-        "attack_pen": 2,
-        "attack_speed": 3,
-        "attack_range": 3,
-        "level": 0,
-        "gold_cost": 10
+    dict(
+        id=1,
+        name="Basic-Tower",
+        icon="tower_1.png",
+        health=100,
+        attack_min=2,
+        attack_max=4,
+        attack_pen=2,
+        attack_speed=3,
+        attack_range=3,
+        level=0,
+        gold_cost=10
 
-    },
-    {
-        "id": 2,
-        "name": "Fast-Tower",
-        "icon": "tower_2.png",
-        "health": 100,
-        "attack_min": 2,
-        "attack_max": 4,
-        "attack_pen": 2,
-        "attack_speed": 4,
-        "attack_range": 3,
-        "level": 0,
-        "gold_cost": 20
-    },
-    {
-        "id": 3,
-        "name": "Faster-Tower",
-        "icon": "lazer_tower.png",
-        "health": 100,
-        "attack_min": 4,
-        "attack_max": 6,
-        "attack_pen": 3,
-        "attack_speed": 5,
-        "attack_range": 3,
-        "level": 1,
-        "gold_cost": 30
-    }
+    ),
+    dict(
+        id=2,
+        name="Fast-Tower",
+        icon="tower_2.png",
+        health=100,
+        attack_min=2,
+        attack_max=4,
+        attack_pen=2,
+        attack_speed=4,
+        attack_range=3,
+        level=0,
+        gold_cost=20
+    ),
+    dict(
+        id=3,
+        name="Faster-Tower",
+        icon="lazer_tower.png",
+        health=100,
+        attack_min=4,
+        attack_max=6,
+        attack_pen=3,
+        attack_speed=5,
+        attack_range=3,
+        level=1,
+        gold_cost=30
+    )
 ]
 
 levelup=[
@@ -78,70 +78,66 @@ levelup=[
 ]
 
 type=[
-    {
-        "id": 0,
-        "name": "Ground",
-        "collision": True
-    },
-    {
-        "id": 1,
-        "name": "Flying",
-        "collision": False
-    }
+    dict(
+        id=0,
+        name="Ground",
+        collision=True
+    ),
+    dict(
+        id=1,
+        name="Flying",
+        collision=False
+    )
 ]
 
-unit = [
-    {
-        "name": "Militia",
-        "icon": "militia.png",
-        "health": 40,
-        "armor": 2,
-        "speed": 1,
-        "type": 0,
-        "level": 0,
-        "gold_drop": 1,
-        "gold_cost": 10
-    },
-    {
-        "name": "Footman",
-        "icon": "footman.png",
-        "health": 80,
-        "armor": 4,
-        "speed": 1,
-        "type": 0,
-        "level": 0,
-        "gold_drop": 1,
-        "gold_cost": 20
-    },
-    {
-        "name": "Grunt",
-        "icon": "grunt.png",
-        "health": 140,
-        "armor": 4,
-        "speed": 1,
-        "type": 0,
-        "level": 0,
-        "gold_drop": 1,
-        "gold_cost": 40
-    },
-    {
-        "name": "Armored Grunt",
-        "icon": "armored_grunt.png",
-        "health": 190,
-        "armor": 6,
-        "speed": 1.2,
-        "type": 0,
-        "level": 0,
-        "gold_drop": 1,
-        "gold_cost": 100
-    }
-
-
+unit =[
+    dict(
+        name="Militia",
+        icon="militia.png",
+        health=40,
+        armor=2,
+        speed=1,
+        type=0,
+        level=0,
+        gold_drop=1,
+        gold_cost=10
+    ),
+    dict(
+        name="Footman",
+        icon="footman.png",
+        health=80,
+        armor=4,
+        speed=1,
+        type=0,
+        level=0,
+        gold_drop=1,
+        gold_cost=20
+    ),
+    dict(
+        name="Grunt",
+        icon="grunt.png",
+        health=140,
+        armor=4,
+        speed=1,
+        type=0,
+        level=0,
+        gold_drop=1,
+        gold_cost=40
+    ),
+    dict(
+        name="Armored Grunt",
+        icon="armored_grunt.png",
+        health=190,
+        armor=6,
+        speed=1.2,
+        type=0,
+        level=0,
+        gold_drop=1,
+        gold_cost=100
+    )
 
 ]
 
 upgrades=[
-    {
-
-    }
+    dict()
 ]
